b051613d1945f0e0d066756300a259753572b177
Alvee3120 <fazlahkarim2002@gmail.com>
2026-09-25 19:31:08 +0600

    first commit

---
+++ decisionmaking.js
@@ -0,0 +1,45 @@
+// If else 
+
+
+let age = 10 ;
+
+if (age >= 18){
+    console.log("voter")
+}else {
+    console.log("not Voter")
+}
+
+// switch case 
+
+let marks = 100 
+
+switch (true) {
+
+    case marks >= 90 :
+        console.log("A+");
+        break;
+    
+    case marks >= 80 :
+        console.log("A");
+        break;
+    
+    case marks >= 70 :
+        console.log("B");
+        break;
+    
+    default:
+        console.log("Fail , Need Improvements");
+        break;
+        
+
+        
+        
+}
+
+
+// ternary 
+
+let age = 10 
+
+age >= 18 ? console.log("voter") : console.log("not voter");
+
