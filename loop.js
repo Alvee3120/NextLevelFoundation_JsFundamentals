87df7719ef46e0ee93efca1db54c28f9fc08c7f0
Alvee3120 <fazlahkarim2002@gmail.com>
2026-09-26 13:14:40 +0600

    loop done

---
+++ loop.js
@@ -0,0 +1,15 @@
+for(let i = 0 ; i <= 5 ; i++){
+    // console.log(i);
+    
+}
+
+for(let i = 5 ; i >= 0 ; i--){
+    // console.log(i);
+    
+}
+
+let i = 0
+while(i < 5){
+    console.log(i);
+    i++;
+}
