74c7aafed77a6591b0b9372b29d87a8aafce6432
Alvee3120 <fazlahkarim2002@gmail.com>
2026-09-26 12:09:38 +0600

    object done

--- object.js
+++ object.js
@@ -1,8 +1,35 @@
 let user = {
 
-    name : "alvee",
+    name : "alvee", // key : value and whole things is  a entity
     age : 25 , 
     address : "Noakhali"
 };
 
 console.log(user);
+
+// delete any key value
+
+delete user.address ;
+
+console.log(user);
+
+// add any key value 
+
+user.address = "Dhaka" ; 
+user.phone = 164 ;
+console.log(user);
+
+//modify any value 
+
+user.address = {
+        District : "Noakhali",
+        SubDistrict: "Sadar", 
+        PostCode : 3804
+};
+
+console.log(user);
+
+
+
+console.log(Object.entries(user));
+console.log(Object.keys(user));
