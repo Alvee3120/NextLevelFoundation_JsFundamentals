ed0ddb467eb9ecc535e8577f3315988ca365488b
Alvee3120 <fazlahkarim2002@gmail.com>
2026-09-26 11:52:07 +0600

    splice

--- array.js
+++ array.js
@@ -20,3 +20,9 @@ console.log(name);
 //shift --> remove from 0 index
 name.shift();
 console.log(name);
+
+
+//splice ( start index , delete count --> how many i want to delete)
+
+name.splice(1,1 , "mango"); // in 1 index , delete 1 items and add mango
+console.log(name);
